35a8204ff89e7012c9d280a96d2a5d84fec0c514
Bob Z Saludo <bob-zs@users.noreply.github.com>
2024-10-08 12:17:32 -0400

    server and webpack hot reload

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const JavaScriptObfuscator = require('webpack-obfuscator');
+
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js', // Your app's main JS file
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      ...(isProduction
+        ? [
+            new JavaScriptObfuscator({
+              compact: true,
+              controlFlowFlattening: true,
+            }),
+          ]
+        : []),
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+  };
+};
