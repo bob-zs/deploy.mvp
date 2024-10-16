75317feb9bcb49638c4273ccef0b421e2198b7c0
Bob Z Saludo <bob-zs@users.noreply.github.com>
2024-10-16 16:33:14 -0400

    update test

--- tests/frontend.test.js
+++ tests/frontend.test.js
@@ -1,12 +1,12 @@
 const { JSDOM } = require("jsdom");
 
 describe("Frontend JavaScript", () => {
-  it("should change the background color to lightgreen", () => {
+  it("should change the background color to orange", () => {
     const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
     global.document = dom.window.document;
 
     require("@src/index");
 
-    expect(document.body.style.backgroundColor).toBe("lightgreen");
+    expect(document.body.style.backgroundColor).toBe("orange");
   });
 });
